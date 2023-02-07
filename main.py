4378efd675a4202bdd01d07fdae0fe89fe547ae4
Dynamo <tomasolbert0412@gmail.com>
2023-02-07 20:28:55 +0100

    finish

---
+++ main.py
@@ -0,0 +1,27 @@
+from numba import jit
+import numpy as np
+from PIL import Image
+
+@jit(nopython=True)
+def floyd_steinberg(image):
+    # image: np.array of shape (height, width), dtype=float, 0.0-1.0
+    # works in-place!
+    h, w = image.shape
+    for y in range(h):
+        for x in range(w):
+            old = image[y, x]
+            new = np.round(old)
+            image[y, x] = new
+            error = old - new
+            # precomputing the constants helps
+            if x + 1 < w:
+                image[y, x + 1] += error * 0.4375 # right, 7 / 16
+            if (y + 1 < h) and (x + 1 < w):
+                image[y + 1, x + 1] += error * 0.0625 # right, down, 1 / 16
+            if y + 1 < h:
+                image[y + 1, x] += error * 0.3125 # down, 5 / 16
+            if (x - 1 >= 0) and (y + 1 < h):
+                image[y + 1, x - 1] += error * 0.1875 # left, down, 3 / 16
+    return image
+
+
